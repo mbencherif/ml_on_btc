42e195779b8951da71b7401f89a5a43f8c770251
nateGeorge <wordsforthewise@gmail.com>
2019-10-20 17:22:52 -0600

    fix indexing issue, fix backtesting

--- predict_bars.py
+++ predict_bars.py
@@ -20,6 +20,61 @@ from keras.callbacks import EarlyStopping, TensorBoard
 
 from bitfinex_ohlc_import import load_candle_data as lcd
 
+
+def create_feats_targs_pct(df, num_hist_bars=25, num_predict_bars=5):
+    """
+    Creates features and targets from raw data.
+    Uses percent change for features and targets.
+    num_hist_bars: number of historical bars to use for predictions
+    num_predict_bars: number of bars in the future to predict
+    """
+    # get pct changes for each OHLCV targets
+    # TODO: try normalizing OHL as ratios to close
+    # TODO: try standardizing targets
+    # low and high need to be ratios to close in order to work properly
+    # or some other way to ensure low is below OC and high is above OC
+    pct_df = df.copy()
+    pct_df = pct_df.pct_change(1)
+
+    # first datapoint will be NA, so drop first row
+    new_df = df.iloc[1:].copy()
+    pct_df = pct_df.iloc[1:]
+
+    # get features and targets -- must be a more elegant way to do this
+    feats = []
+    targs = []
+    # raw_feats = []
+    # raw_targs = []
+    for i in tqdm(range(num_hist_bars, pct_df.shape[0] - num_predict_bars)):
+        feats.append(pct_df.iloc[i - num_hist_bars:i].values.flatten())
+        targs.append(pct_df.iloc[i:i + num_predict_bars].values.flatten())
+
+        # raw_feats.append(new_df.iloc[i - num_hist_bars:i].values.flatten())
+        # raw_targs.append(new_df.iloc[i:i + num_predict_bars].values.flatten())
+
+    feats = np.array(feats)
+    targs = np.array(targs)
+    # raw_feats = np.array(raw_feats)
+    # raw_targs = np.array(raw_targs)
+
+    return new_df, feats, targs
+
+
+def create_train_test(feats, targs, train_frac=0.8):
+    """
+    train_frac: float between 0 and 1, fraction of dataset to use for training
+    """
+    train_idx = int(train_frac * feats.shape[0])
+
+    train_feats = feats[:train_idx]
+    train_targs = targs[:train_idx]
+
+    test_feats = feats[train_idx:]
+    test_targs = targs[train_idx:]
+
+    return train_feats, train_targs, test_feats, test_targs
+
+
 def create_simple_model(feats, targs):
     model = Sequential()
     model.add(BatchNormalization(input_shape=feats[0].shape))
@@ -39,7 +94,6 @@ def create_simple_model(feats, targs):
     model.compile(loss='mean_absolute_error', optimizer='sgd', metrics=['mse'])
 
     return model
-
 
 
 def ensure_low_high(df):
@@ -87,21 +141,29 @@ def test_backcalc(df, targs, num_hist_bars, num_predict_bars):
     df: pd.DataFrame of original data
     targs: np.ndarray of target values, should be percent changes
     """
-    rs_targs = targs.reshape(-1, num_predict_bars, num_predict_bars)
-    first_point = df.iloc[num_hist_bars - 1]
-    first_point * (1 + rs_targs[0][0])
+    # from prototyping
 
-    preds = [first_point * (1 + rs_targs[0][0])]
-    for i in range(1, num_predict_bars):
-        preds.append(preds[-1] * (1 + rs_targs[0][i]))
+    # rs_targs = targs.reshape(-1, num_predict_bars, num_predict_bars)
+    # first_point = df.iloc[num_hist_bars - 1]
+    # first_point * (1 + rs_targs[0][0])
 
-    pr = pd.concat(preds, axis=1).T
+    # preds = [first_point * (1 + rs_targs[0][0])]
+    # for i in range(1, num_predict_bars):
+    #     preds.append(preds[-1] * (1 + rs_targs[0][i]))
+
+    # pr = pd.concat(preds, axis=1).T
+
     # backcalculate all values from targets
-    all_preds = []
-    for i in tqdm(range(rs_targs.shape[0])):
-        point = df.iloc[i + num_hist_bars - 1]
-        pr = back_calc_one_point_pct(point, rs_targs[i])
-        all_preds.append(pr)
+    # all_preds = []
+    # for i in tqdm(range(rs_targs.shape[0])):
+    #     point = df.iloc[i + num_hist_bars - 1]
+    #     pr = back_calc_one_point_pct(point, rs_targs[i])
+    #     all_preds.append(pr)
+
+    all_preds = backcalculate_predictions(df,
+                                        targs,
+                                        num_hist_bars=num_hist_bars,
+                                        num_predict_bars=num_predict_bars)
 
     # test that calculations are working as expected
     for i in tqdm(range(len(all_preds))):
@@ -128,65 +190,21 @@ def backcalculate_predictions(df, predictions, num_hist_bars, num_predict_bars):
     return all_preds
 
 
-def create_feats_targs_pct(df, num_hist_bars=25, num_predict_bars=5):
-    """
-    Creates features and targets from raw data.
-    Uses percent change for features and targets.
-    num_hist_bars: number of historical bars to use for predictions
-    num_predict_bars: number of bars in the future to predict
-    """
-    # get pct changes for each OHLCV targets
-    # TODO: try normalizing OHL as ratios to close
-    # TODO: try standardizing targets
-    # low and high need to be ratios to close in order to work properly
-    # or some other way to ensure low is below OC and high is above OC
-    pct_df = df.copy()
-    pct_df = pct_df.pct_change(1)
 
-    # first datapoint will be NA, so drop first row
-    new_df = df.iloc[1:].copy()
-    pct_df = pct_df.iloc[1:]
-
-    # get features and targets -- must be a more elegant way to do this
-    feats = []
-    targs = []
-    raw_feats = []
-    raw_targs = []
-    for i in tqdm(range(num_hist_bars, pct_df.shape[0] - num_hist_bars - num_predict_bars)):
-        feats.append(pct_df.iloc[i - num_hist_bars:i].values.flatten())
-        targs.append(pct_df.iloc[i:i + num_predict_bars].values.flatten())
-
-        raw_feats.append(new_df.iloc[i - num_hist_bars:i].values.flatten())
-        raw_targs.append(new_df.iloc[i:i + num_predict_bars].values.flatten())
-
-    feats = np.array(feats)
-    targs = np.array(targs)
-    raw_feats = np.array(raw_feats)
-    raw_targs = np.array(raw_targs)
-
-    return feats, targs
-
-
-def create_train_test(feats, targs, train_frac=0.8)
-    """
-    train_frac: float between 0 and 1, fraction of dataset to use for training
-    """
-    train_idx = int(train_frac * feats.shape[0])
-
-    train_feats = feats[:train_idx]
-    train_targs = targs[:train_idx]
-
-    test_feats = feats[train_idx:]
-    test_targs = targs[train_idx:]
-
-    return train_feats, train_targs, test_feats, test_targs
-
+# set parameters
+train_frac = 0.8
+num_hist_bars = 25
+num_predict_bars = 5
 
 # load data
 df = lcd.load_data(candle_size='1m')
 df_1h = lcd.resample_data(df, '1H')
 
-feats, targs = create_feats_targs_pct(df_1h)
+new_df, feats, targs = create_feats_targs_pct(df_1h,
+                                            num_hist_bars=num_hist_bars,
+                                            num_predict_bars=num_predict_bars)
+
+train_feats, train_targs, test_feats, test_targs = create_train_test(feats, targs, train_frac=train_frac)
 
 # make model, evaluate performance
 model = create_simple_model(feats, targs)
@@ -204,6 +222,19 @@ With pct feats and targs, seems to do much better
 
 train_preds = model.predict(train_feats)
 
-test_backcalc(df_1h, targs, num_hist_bars, num_predict_bars)
+# should print nothing if works properly, otherwise prints 'uh oh..'
+# test_backcalc(new_df, targs, num_hist_bars, num_predict_bars)
 
-true_preds = backcalculate_predictions(df_1h, train_preds, num_hist_bars, num_predict_bars)
+true_preds = backcalculate_predictions(new_df, train_preds, num_hist_bars, num_predict_bars)
+
+
+# just take first bar predcition for plotting
+first_bars = [t.iloc[0] for t in true_preds]
+
+first_bars_df = pd.concat(first_bars, axis=1).T
+
+train_idx = int(train_frac * feats.shape[0])
+
+# starts one after hist bars, and goes to num_predict_bars before train_idx
+# need to remove second num_hist_bars when re-running
+true_train_targs = new_df.iloc[num_hist_bars + 1:train_idx - num_predict_bars]
